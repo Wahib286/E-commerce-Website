43e12bdf04f83ffe85ef478bed0400289d3ad250
Wahib <wahibbashar.it25@jecrc.ac.in>
2025-04-30 15:31:41 +0530

    Final

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Spinner = () => {
   return (
-    <div className='loader'></div>
+    <div className='h-screen w-screen flex justify-center items-center'>
+    <div className='loader ' ></div>
+    </div>
   )
 }
